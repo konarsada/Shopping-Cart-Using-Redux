749c22c4e4ea46f346e126c0fb728a8c5cb5c30b
konarsada <sadabratak@gmail.com>
2022-06-24 03:15:35 +0530

    login registration

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
     
     return (
         <div className="App">
-            {/*!isLoggedIn && <Auth />*/}
-            {isLoggedIn && <Layout />}
+            {/*!isLoggedIn && <Auth />}
+            {isLoggedIn && <Layout />*/}
+            <Layout />
         </div>
     )
 }
